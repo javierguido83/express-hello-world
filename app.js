0d5813905cd7ceb5fddef51fe72655830e7adcf0
javierguido83 <93479907+javierguido83@users.noreply.github.com>
2023-07-03 17:35:48 -0300

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ server.headersTimeout = 120 * 1000;
 app.get("/menu", function (req, res) {
    fs.readFile("/" + "menu.json", 'utf8', function (err, data) {
       console.log( data );
-      res.end( data );
+      res.json( data );
    });
 });
 
